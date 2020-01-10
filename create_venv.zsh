cba6d941fe9075fcaee9dea4f7c22fd45010e61c
nikita-loik <nikita.loik@gmail.com>
2020-01-10 08:50:45 +0100

    Update create_venv.zsh script

--- create_venv.zsh
+++ create_venv.zsh
@@ -1,8 +1,20 @@
 #!/bin/zsh
-echo "===CREATING VIRTUAL ENVIRONMENT==="
-virtualenv -p python3 venv_route_inspection
-source venv_route_inspection/bin/activate
+# A. Get virtual environment name.
+# A.1. Get working directory name (without full path).
+dir_name=${PWD##*/}
+# A.2. Replace '-' with '_' in a name.
+venv_stem="${dir_name//-/_}"
+# A.3. Get virtual environment name.
+venv_name="venv_${venv_stem}"
+echo "===creating virtual environment ${venv_name}==="
+# B. Create virtual environment.
+virtualenv -p python3 ".${venv_name}"
+# C. Activate virtual environment.
+source ".${venv_name}/bin/activate"
+# D. Install jupyter.
 pip install jupyter
-python -m ipykernel install --user
+# E. Install IPython kernel.
+python -m ipykernel install --user --name="${venv_name}"
+# F. Install requirements.
 pip install -r requirements.txt
-echo "===VIRTUAL ENVIRONMENT CREATED==="
+echo "===virtual environment created==="
